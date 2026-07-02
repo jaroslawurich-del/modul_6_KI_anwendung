b030215ef885220814ec13ebfb3d7f5a2925f0b2
jaroslawurich <jaroslaw.urich@gmail.com>
2026-07-02 10:52:22 +0200

    added day 4 of week 22

---
+++ week_22/DAY_4/agent_tutorial_v3.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 29 10:58:21 2025
+
+@author: milos
+"""
+
+from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
+from langchain_tavily import TavilySearch
+
+# Setup LLM
+llm = ChatOllama(model='gpt-oss:120b-cloud')
+
+# Setup search tool
+search = TavilySearch(
+    k=3,
+    tavily_api_key="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"
+)
+
+tools = [search]
+
+
+# Create React agent
+agent = create_agent(model=llm, 
+                     tools=tools, 
+                     system_prompt="You are a helpful assistant. Be concise and accurate.")
+
+    
+# Infinite loop to continuously accept user questions from the terminal
+while True:
+    # Prompt for user input (the question to ask the agent)
+    query = input("\nYou: ")
+
+    # If the input is "exit" or "quit" (case-insensitive), break the loop and end the session
+    if query.lower() in ["exit", "quit"]:
+        print("Goodbye!")
+        break
+
+    # The agent will use the tools and system prompt defined earlier
+    for step in agent.stream(
+        {"messages": [{"role": "user", "content": query}]},
+        stream_mode="values",
+    ):
+        step["messages"][-1].pretty_print()
+
